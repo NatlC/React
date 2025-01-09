08c681f37cb4e14c4f2680bc468dc3e918a2081a
NatlC <nathancar.go@hotmail.com>
2025-01-09 03:31:38 -0500

    g

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const handleScroll = () => {
+  const sections = document.querySelectorAll('.section-container');
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const triggerBottom = window.innerHeight * 0.8;
+    if (sectionTop < triggerBottom) {
+      section.classList.add('scroll-in');
+    }
+  });
+};
+
+window.addEventListener('scroll', handleScroll);
+
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
@@ -13,7 +27,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
