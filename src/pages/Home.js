9b61f4182cb2a774c8bee8427bb4f277d9bcdf9f
NatlC <nathancar.go@hotmail.com>
2025-01-14 11:33:31 -0500

    made everything more responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -200,7 +200,6 @@ const Home = () => {
         </div>
         <div className="section-text">
           <br></br>
-          <br></br>
           <h3>Arcade Game Application, 2021</h3>
           <p>• Developed an interactive arcade-style game with storyboard, mockup, and prototype.</p>
           <p>• Lead bi-weekly team calls with agile project management to ensure development and testing complete on time with good quality and performance.</p>
