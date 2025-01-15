749e0f2787b63578b985d82d430d5d2460222add
NatlC <nathancar.go@hotmail.com>
2025-01-14 23:10:56 -0500

    fixed resume link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 // src/components/Navigation.js
-// src/components/Navigation.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import pdfResume from '../assets/2025 Nathan chan resume.pdf'; // Adjust the path to your PDF file
 
 const Navigation = () => {
   return (
     <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About Me</Link>
-      <a href="https://www.linkedin.com/in/nathanl-chan/overlay/1735796127893/single-media-viewer/?profileId=ACoAADlZSjYBCcpr5Fs6s2rUr7DKhKfPTM3jb_g" target="_blank" rel="noopener noreferrer">Resume</a>
+      <a href={pdfResume} target="_blank" rel="noopener noreferrer">Resume</a> {/* Link to the PDF resume */}
     </nav>
   );
 };
